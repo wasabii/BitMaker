5732cd76963fbb86594dfbc92ab7510c37766333
Jerome Haltom <wasabi@larvalstage.net>
2011-06-19 21:23:11 -0500

    * Moved all the utilities to a central project. * SHA-256 implementation is now complete. * SHA-256 test. * Still need to clean up the CPU thing again.

---
+++ BitMaker.Utils.Tests/MemoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitMaker.Utils.Tests
+{
+
+    [TestClass()]
+    public class MemoryTest
+    {
+
+        [TestMethod()]
+        public void ReverseEndianTestUInt32()
+        {
+            uint value = 63335;
+            uint expected = 1744240640;
+            uint actual;
+            actual = Memory.ReverseEndian(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ReverseEndianTestUInt64()
+        {
+            ulong value = 63335;
+            ulong expected = 7491456505154109440;
+            ulong actual;
+            actual = Memory.ReverseEndian(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+}
---
+++ BitMaker.Utils.Tests/Sha256Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
+
+namespace BitMaker.Utils.Tests
+{
+
+    [TestClass]
+    public class Sha256Test
+    {
+
+        [TestMethod]
+        public unsafe void StringHashTest()
+        {
+            var data = Encoding.UTF8.GetBytes("abc");
+            var work = Sha256.AllocateInputBuffer(data.Length);
+            var state = new uint[Sha256.SHA256_STATE_SIZE];
+            var hash = new byte[Sha256.SHA256_HASH_SIZE];
+
+            // reference data for comparison against
+            var refHash = System.Security.Cryptography.SHA256.Create().ComputeHash(data);
+
+            fixed (byte* dataPtr = data, workPtr = work, hashPtr = hash)
+            fixed (uint* statePtr = state)
+            {
+                // create working copy of data
+                Memory.Copy(dataPtr, workPtr, data.Length);
+
+                // initialize state with starting values
+                Sha256.Initialize(statePtr);
+                Assert.AreEqual(((uint*)statePtr)[0], 0x6a09e667U);
+                Assert.AreEqual(((uint*)statePtr)[7], 0x5be0cd19U);
+
+                for (int i = 0; i < work.Length / Sha256.SHA256_BLOCK_SIZE; i++)
+                {
+                    Sha256.Prepare(workPtr, data.Length, i);
+                    Sha256.Transform(statePtr, workPtr + i * Sha256.SHA256_BLOCK_SIZE);
+                }
+
+                // output hash
+                Sha256.Finalize(statePtr, hashPtr);
+            }
+
+            for (int i = 0; i < hash.Length; i++)
+                Assert.AreEqual(refHash[i], hash[i]);
+        }
+
+    }
+
+}
